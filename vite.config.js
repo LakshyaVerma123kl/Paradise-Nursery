0ce1e28d06573a77daecc673ded46c58dcd9f2f0
LakshyaVerma123kl <142150397+LakshyaVerma123kl@users.noreply.github.com>
2024-08-25 16:52:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Paradise-Nursery/", // Set this to your repository name
   plugins: [react()],
+  base: '/', // Make sure this is set correctly
 });
