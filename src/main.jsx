dc94562f1ec3e4a6eafa906143f4313e6e8f9a35
LakshyaVerma123kl <142150397+LakshyaVerma123kl@users.noreply.github.com>
2024-08-25 16:57:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Updated import for React 18
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import LandingPage from "./LandingPage.jsx";
 import ProductListing from "./ProductListing.jsx";
 import ShoppingCart from "./ShoppingCart.jsx";
-import Checkout from "./Checkout.jsx"; // Don't forget to import the Checkout component if you're using it
+import Checkout from "./Checkout.jsx";
 
 const App = () => (
   <Router basename="/Paradise-Nursery/">
@@ -13,12 +13,12 @@ const App = () => (
       <Route path="/" element={<LandingPage />} />
       <Route path="/products" element={<ProductListing />} />
       <Route path="/cart" element={<ShoppingCart />} />
-      <Route path="/checkout" element={<Checkout />} /> {/* Checkout route */}
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   </Router>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root for React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
