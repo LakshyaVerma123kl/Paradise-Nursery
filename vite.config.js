5d07e7649d519a997520433400177b6951a4029d
LakshyaVerma123kl <142150397+LakshyaVerma123kl@users.noreply.github.com>
2024-08-25 16:54:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is set correctly
+  build: {
+    outDir: 'dist',
+    base: '/Paradise-Nursery/', // Matches the basename in your Router
+  },
 });
